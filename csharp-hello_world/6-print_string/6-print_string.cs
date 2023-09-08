81958d05a28033f5d3a76383f7645cd6ebb9e891
Night8guy <robert@lionsroad.com>
2023-09-07 23:47:35 -0500

    tasks 5 and 6

---
+++ csharp-hello_world/6-print_string/6-print_string.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _6_print_string
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Holberton School";
+            Console.WriteLine($"{str}{str}{str}");
+            Console.WriteLine(str.Substring(0, 9));
+        }
+    }
+}
