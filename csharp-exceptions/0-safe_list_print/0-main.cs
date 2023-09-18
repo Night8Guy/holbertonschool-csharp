fb5b6641bc1bff47384ccf525210e8d7eb661003
Night8guy <robert@lionsroad.com>
2023-09-17 20:30:29 -0500

    deleting files, trying netcore again

---
+++ csharp-exceptions/0-safe_list_print/0-main.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<int> myList = new List<int>() {1, 2, 3, 4, 5};
+        int count;
+
+        count = List.SafePrint(myList, myList.Count);
+        Console.WriteLine("Number of integers printed: " + count);
+        Console.WriteLine();
+
+        count = List.SafePrint(myList, myList.Count - 2);
+        Console.WriteLine("Number of integers printed: " + count);
+        Console.WriteLine();
+
+        count = List.SafePrint(myList, myList.Count + 2);
+        Console.WriteLine("Number of integers printed: " + count);
+    }
+}
+carrie@ubuntu:~//0-safe_list_print$
+carrie@ubuntu:~//0-safe_list_print$ ls
+0-main.cs  0-safe_list_print.cs  0-safe_list_print.csproj  bin  obj
+carrie@ubuntu:~//0-safe_list_print$ dotnet run
+1
+2
+3
+4
+5
+Number of integers printed: 5
+
+1
+2
+3
+Number of integers printed: 3
+
+1
+2
+3
+4
+5
+Number of integers printed: 5
