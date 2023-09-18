cbcdb2dc4ffac3d24d9ed92ffd488a2b6d857b8a
Night8guy <robert@lionsroad.com>
2023-09-17 23:22:46 -0500

    hello world tasks pre dotnet

--- csharp-hello_world/4-print_integer/4-print_integer.cs
+++ csharp-hello_world/4-print_integer/4-print_integer.cs
@@ -5,6 +5,6 @@ class Program
     static void Main(string[] args)
     {
         int number = 972;
-        Console.WriteLine(number + " Mission street");
+        Console.WriteLine(number + " Mission Street");
     }
 }
