add14035a567d974d39b2b650932571212d047f7
Robert E. Hollis Jr <112408198+Night8Guy@users.noreply.github.com>
2024-01-18 10:40:28 -0600

    Delete csharp-ifelse_loops_methods directory

--- csharp-ifelse_loops_methods/7-islower/7-islower.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-public class Character
-{
-    public static bool IsLower(Char c)
-    {
-        return (c >= 'a' && c <= 'z');
-    }
-}
--- csharp-ifelse_loops_methods/9-add/9-add.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-public class Number
-{
-    public static int Add(int a, int b)
-    {
-        return (a + b);
-    }
-}
